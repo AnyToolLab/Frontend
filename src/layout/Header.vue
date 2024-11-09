fb0f4808527485064c946bdefb9b21ed9b962bba
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-11-09 22:52:21 +0200

    Added image converter

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -31,62 +31,61 @@ import ArrowDownIMG from '../assets/images/arrow-down.svg'
 </template>
 
 <style scoped>
-    .header {
-        background-color: #01257D;
-        height: 84px;
-        color: #FEFDF9;;
-    }
+.header {
+    background-color: #01257D;
+    height: 84px;
+    color: #FEFDF9;
+}
 
-    .header__content {
-        display: flex;
-        justify-content: space-between;
-        height: 84px;
-        align-items: center;
-    }
+.header__content {
+    display: flex;
+    justify-content: space-between;
+    height: 84px;
+    align-items: center;
+}
 
-    .header__logo-text {
-        font-weight: bold;
-        font-size: 24;
-    }
+.header__logo-text {
+    font-weight: bold;
+    font-size: 24;
+}
 
-    .header__tools {
-        margin-right: 130px;
-        cursor: pointer;
-        position: relative;
-    }
+.header__tools {
+    margin-right: 130px;
+    cursor: pointer;
+    position: relative;
+}
 
-    .header__tools:hover {
-        z-index: 100;
-        .header__tools-menu {
-            position: absolute;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            top: 40px;
-            right: -68px;
-        }
-    }
-    
-    .header__tools-text {
-    }
-    
-    .header__tools-img {
-        padding-left: 16px;
-    
-    }
-    
+.header__tools:hover {
+    z-index: 100;
+
     .header__tools-menu {
-        display: none;
-        background-color: #FEFDF9;
-        color: #202020;
-        font-size: 20px;
-        font-weight: 500;
-
-        height: 308px;
-        width: 210px;
-        border-radius: 20px;
-        padding: 24px 24px 24px 46px;
-        border: 1px solid #01257D;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        top: 40px;
+        right: -68px;
     }
+}
 
+.header__tools-text {}
+
+.header__tools-img {
+    padding-left: 16px;
+
+}
+
+.header__tools-menu {
+    display: none;
+    background-color: #FEFDF9;
+    color: #202020;
+    font-size: 20px;
+    font-weight: 500;
+
+    height: 308px;
+    width: 210px;
+    border-radius: 20px;
+    padding: 24px 24px 24px 46px;
+    border: 1px solid #01257D;
+}
 </style>
